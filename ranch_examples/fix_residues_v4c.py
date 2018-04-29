1978f3e612da862054d99e092163edff83a1a349
Francisco Javier Guzman <36437485+javierguzm@users.noreply.github.com>
2018-04-29 17:40:37 +0300

    Modified fix_residues script to handle more than two chains

--- ranch_examples/fix_residues_v4c.py
+++ ranch_examples/fix_residues_v4c.py
@@ -1,6 +1,5 @@
 import re
 
-n_chains = 2
 filenames = []
 
 with open("filenames.txt") as f:
@@ -41,9 +40,9 @@ for filename in filenames:
 	chains = []		# list for each chain atom entries
 	hetatm = []		# list for each chain hetatm entries
 	res_chain = []	# list for last residue in each chain
-	chain_identif = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']	# list for chain identifiers
+	chain_identif = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']	
+		# list for chain identifiers
 
-	ncount = 0
 	index = 0		# index for the entry number
 
 	for i in range(nchains):
@@ -55,11 +54,12 @@ for filename in filenames:
 		chainl = chainsl[i]		# length of the current chain
 		cacount = 0		# counts the residues
 
-		# Storing the chains in separate elements of 'chains' list
+		# Storing the chains in separate elements of 'chains' list,
+		# with their own chain identifier
 		for j in range(index,len(data)):
 			index += 1
 			
-			char = re.search(r'[A-Z][A-Z][A-Z] [AB] ', data[j]).group()
+			char = re.search(r'[A-Z][A-Z][A-Z] [A-Z] ', data[j]).group()
 			# if NAP, add to hetatm list with the appropriate chain identifier
 			if re.search(r'NAP', data[j]):
 				hetatm[i].append(data[j].replace(char, char[0:4]+chain_identif[i]+' '))
@@ -91,38 +91,46 @@ for filename in filenames:
 
 	for i in range(nchains):		
 		# Replace entry number and residue sequence number
-		res_old = chains[i][0][23:26]	# initialize to the first residue number (should be 1 in most cases)
+		res_old = chains[i][0][23:26]	# initialize to the first residue number 
+										# (should be 1 in most cases)
 		res_new = 1
 		for j in range(len(chains[i])):
 
 			entry_num = re.search(r'ATOM\s+\d+', chains[i][j])
-			entry_res = re.search(r'[A-Z][A-Z][A-Z]\s[AB]\s+' + res_old, chains[i][j])
+			entry_res = re.search(r'[A-Z][A-Z][A-Z]\s[A-Z]\s+' + res_old, chains[i][j])
 			
 			if entry_res:
 				# replace residue number
-				chains[i][j] = chains[i][j].replace(entry_res.group(), entry_res.group()[0:5] + str(res_new).rjust(4))
+				chains[i][j] = chains[i][j].replace(entry_res.group(), 
+					entry_res.group()[0:5] + str(res_new).rjust(4))
 				# replace entry number
-				chains[i][j] = chains[i][j].replace(entry_num.group(), entry_num.group()[0:5] + str(n).rjust(6))
+				chains[i][j] = chains[i][j].replace(entry_num.group(), 
+					entry_num.group()[0:5] + str(n).rjust(6))
 				n += 1
 				continue
 			
 			# If the residue number changed (new residue)
 			res_old = chains[i][j][23:26]
 			res_new += 1
-			entry_res = re.search(r'[A-Z][A-Z][A-Z]\s[AB]\s+' + res_old, chains[i][j])
+			entry_res = re.search(r'[A-Z][A-Z][A-Z]\s[A-Z]\s+' + res_old, chains[i][j])
 			if entry_res:
 				# replace residue number
-				chains[i][j] = chains[i][j].replace(entry_res.group(), entry_res.group()[0:5] + str(res_new).rjust(4))
+				chains[i][j] = chains[i][j].replace(entry_res.group(), 
+					entry_res.group()[0:5] + str(res_new).rjust(4))
 				# replace entry number
-				chains[i][j] = chains[i][j].replace(entry_num.group(), entry_num.group()[0:5] + str(n).rjust(6))
+				chains[i][j] = chains[i][j].replace(entry_num.group(), 
+					entry_num.group()[0:5] + str(n).rjust(6))
 				n += 1
 			else:
-				raise AttributeError(str(n) + " : chain " + str(i) + ", entry " + str(j) + ", res_old: "+res_old)
+				print chains[i][j]
+				raise AttributeError(str(n) + " : chain " + str(i) + 
+					", entry " + str(j) + ", res_old: "+res_old)
 
 		# Add TER entry at the end of the chain
 		# FIX FOR CHAINS WITH MORE THAN 1000 RESIDUES
-		aa = re.search(r'[A-Z][A-Z][A-Z]\s[AB]\s+', chains[i][-1])
-		chains[i].append('TER    ' + str(n) + aa.group()[0:3].rjust(9) + aa.group()[4].center(3) + str(res_new))
+		aa = re.search(r'[A-Z][A-Z][A-Z]\s[A-Z]\s+', chains[i][-1])
+		chains[i].append('TER    ' + str(n) + aa.group()[0:3].rjust(9) + 
+			aa.group()[4].center(3) + str(res_new))
 		n += 1
 
 		# last residue number in the current chain, plus 2 to add the hetatm entries
@@ -138,10 +146,12 @@ for filename in filenames:
 		res_new = res_chain[i]
 		for j in range(len(hetatm[i])):
 			entry_num = re.search(r'ATOM\s+\d+', hetatm[i][j])
-			entry_res = re.search(r'NAP [AB]\s+\d+', hetatm[i][j])
+			entry_res = re.search(r'NAP [A-Z]\s+\d+', hetatm[i][j])
 
 			# replace residue number and entry number
-			complete.append(hetatm[i][j].replace(entry_res.group(), entry_res.group()[0:5] + str(res_new).rjust(4)).replace(entry_num.group(), 'HETATM' + str(n).rjust(5)))
+			complete.append(hetatm[i][j].replace(entry_res.group(), 
+				entry_res.group()[0:5] + str(res_new).rjust(4)).replace(entry_num.group(), 
+				'HETATM' + str(n).rjust(5)))
 			n += 1
 
 	##print ''
