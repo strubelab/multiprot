8a4e2c2430d6f609eedead5b651bf5851a3fc5b9
Francisco Javier Guzman <36437485+javierguzm@users.noreply.github.com>
2018-06-06 11:11:04 +0300

    Added another case to trimer_multich example; Added whiteboard pictures; First steps of diagram implemented in new_ranch.py

--- multiprot/new_ranch.py
+++ multiprot/new_ranch.py
@@ -9,6 +9,7 @@ Last edited: 20/feb/18
 import biskit as B
 import numpy as N
 import tempfile, os
+import re
 
 from biskit.exe.executor import Executor
 import biskit.tools as t
@@ -20,9 +21,11 @@ class Ranch( Executor ):
     pdbs_in = ()
     sequence = ''
 
-"""
-    def __init__(self, *domains, chains={}):
 
+    def __init__(self, *domains, chains={}, symmetry='p1', symtemplate=None, fixed=None, multich=None):
+        # Raise exception if symmetry is different than p1 and symtemplate is None,
+        # and if symtemplate is not in domains
+        
         # Create temporary folder for pdbs and sequence
         tempdir = tempfile.mktemp( '', self.__class__.__name__.lower() + '_', t.tempDir() )
 
@@ -39,8 +42,137 @@ class Ranch( Executor ):
 
         self.domains = domains
         self.sequence = ''
+        self.symmetry = symmetry
+        self.symtemplate = symtemplate
+        self.doms_in = []
+        self.fixed = fixed
+        self.multich = multich
+        self.embedded = []
+
+        if self.fixed == None:
+            self.fixed = []
+            for element in self.domains:
+                if isinstance(element, B.PDBModel):
+                    self.fixed.append('no')
+
+        if self.multich == None:
+            self.multich = []
+            for element in self.domains:
+                if isinstance(element, B.PDBModel):
+                    if element == self.symtemplate:
+                        self.multich.append('yes')
+                    else:
+                        self.multich.append('no')
 
         Executor.__init__(self, 'ranch', tempdir=tempdir, args=args, cwd=tempdir)
+
+
+    def _setup(self):
+        """
+        - Creates the sequence from the domains and linkers
+        - Creates new PDBModels for ranch input if necessary (which are later
+           converted into pdb files by prepare method)
+        """
+        
+        ######## DIAGRAM STARTS ########
+
+        i = 0   # Counter for domain position
+        for element in self.domains:
+            if isinstance(element, str):    # 1
+                # If is sequence, add to sequence and continue
+                self.sequence += element
+                continue
+
+            elif isinstance(element, B.PDBModel):
+                # if is PDBModel
+
+                if self.multich[i]=='yes' or element.lenChains()==1:    # 2
+                    # If is symmetry core or single domain
+                    # Find a way to make the symmetry test only once?
+                    # Action: Add to sequence and to domains list
+                    self.sequence += element.sequence()
+                    self.doms_in.append(element)
+                    i += 1
+
+                else:  
+                    # Not symmetric, and part of a multichain complex
+                    ## Look only at domain that corresponds to this chain
+
+                    # Get chain index
+                    chain_id = chains[element]
+                    mask_chain = element.maskFrom('chain_id', chain_id)
+                    i_mask_chain = N.nonzero(mask_chain)[0]
+                    chain_ind = element.atom2chainIndices(i_mask_chain)[0]
+
+                    if self.fixed[i] == 'yes':  # 3
+                        # Already modeled, paired with another chain
+                        # THE HIGHER LEVEL PROGRAM MUST GIVE THE ENTIRE MODEL
+                        # AS ONE OF THE FIXED DOMAINS, TO AVOID STEPS 4 AND 5
+                        # IN DIAGRAM
+                        # CONTINUE IN NEW_RANCH_2 WITHOUT STEP 5
+
+                        if element in self.embedded:    # 5
+                            # If the domain is included in the embedded domains,
+                            # meaning that the paired chain is already embedded
+                            # in a previous domain ... is this the best way?
+
+                            # Action: Take fixed domain only as a separate
+                            # PDBModel, add to sequence and domains list
+                            m = element.takeChains([chain_ind])
+                            self.sequence += m.sequence()
+                            self.doms_in.append(m)
+
+                        else:
+                            # The paired chain is not embedded in a previous domain
+                            m = element.takeChains([chain_ind])
+                            embedded = self.embed(m,element)    
+
+                            # AT THE END SAVE HOW THE SEQUENCE SHOULD LOOK, TO HELP FIND SYMMETRIC UNIT
+
+            else:
+                raise TypeError(
+                    'The *domains arguments must be either strings or PDBModels')
+
+    @staticmethod
+    def embed(self, dom, full):
+        '''
+        Embeds one model (full - possibly with multiple chains) into another 
+        (dom) to trick ranch to treat them as a simple single-chain domain
+
+        :param dom: model of a single chain domain that will contain the
+                       other model
+        :type dom: PDBModel
+        :param full: model of a single or multiple chain domain, that generally
+                     contains also the 'dom' domain, and will be embedded
+                     into 'dom'
+        :type full: PDBModel
+        '''
+
+        # Take dom from full
+        start = dom.sequence()[0:10]
+        if re.search(start, full.sequence()):
+            # If the dom sequence is inside full sequence
+            matches = re.finditer(start, full.sequence())
+            first_res_dom = dom.res2atomindices([0])
+            lowdom = first_res_dom[0]
+            highdom = first_res_dom[-1]
+            
+            for match in matches:
+                index = match.start()
+                first_res_full = full.res2atomindices([index])
+
+                lowfull = first_res_full[0]
+                highfull = first_res_full[-1]
+
+                if N.all(dom.xyz[lowdom:highdom+1] == full.xyz[lowfull:highfull+1]):
+                    # If the atoms for the first residue are in the same positions
+                    # Take chain
+                    chain_ind = full.atom2chainIndices(first_res_full)
+                    chains_to_take = list(range(full.lenChains()))
+                    chains_to_take = chains_to_take.remove(chain_ind[0])
+
+                    full = full.takeChains(chains_to_take)
+
 
     def prepare(self):
         """
@@ -53,16 +185,6 @@ class Ranch( Executor ):
         if not os.path.exists(self.tempmodels):
             os.mkdir( self.tempmodels )
 
-        ######## DIAGRAM STARTS ########
-
-        for element in self.domains:
-            if isinstance(element, str):
-                self.sequence += element
-            else:
-                if element.symcore:   ### CREATE THIS VARIABLE... FIND A WAY TO MAKE THIS CHECK ONLY ONCE??
-                    self.sequence += element
-                    self.sym = 'p' + string(self.countchains())
-
 
 
     def cleanup(self):
@@ -73,21 +195,24 @@ class Ranch( Executor ):
             t.tryRemove(self.f_seq)
             for file in self.pdbs_in:
                 t.tryRemove(file)
-        Executor.cleanup(self)
 
+        super().cleanup(self)
 
     def finish(self):
         """
         Overrides Executor method.
+        Write more....
         """
+        # CHECK IF IS CALLED ONLY IF SUCCESSFUL
         Executor.finish(self)
 
         ### Retrieve models created as PDBModels
         m_paths = [os.path.join(self.tempmodels, f) for f in os.listdir(self.tempmodels)]
         self.result = [B.PDBModel(m) for m in m_paths]
 
+
       ## Fix the models
-"""
+
 
 #############
 ##  TESTING        
@@ -97,50 +222,69 @@ import biskit.test as BT
 class TestRanch(BT.BiskitTest):
     """Test class"""
 
-    TAGS = [ BT.EXE ]
+    TAGS = [ BT.EXE, BT.LONG ]
 
     ## Write tests for each case in the ranch examples folder
 
     def prepare(self):
-        # Create PDBModels from pdb files
-        dom1 = B.PDBModel("/Users/guzmanfj/Documents/Stefan/multiprot/ranch_examples/1/2z6o_mod.pdb")
-        dom2 = B.PDBModel("/Users/guzmanfj/Documents/Stefan/multiprot/ranch_examples/1/Histone_H3.pdb")
-        domAB1 = B.PDBModel("/Users/guzmanfj/Documents/Stefan/multiprot/ranch_examples/4/dom1_AB.pdb")
-        domAB2 = domAB1.clone()
-        
-        # Call ranch for exmaple 1 in ranch_examples
-        call_2z60_linker_histone = r.Ranch(dom1, 'GGGGGGGGGG', dom2)
-        models1 = call_2z60_linker_histone.run()
-        
-        # Call ranch for example 4 in ranch_examples
-        call_domAB1_linker_domAB2 = r.Ranch(domAB1, 'GGGGGGGGGG', domAB2, 
-            chains = {domAB1:'A', domAB2:'B'})
-        models2 = call_domAB1_linker_domAB2.run()
-
-        # example 5 in ranch_examples
-        call_domAB1_linker_domAB2 = r.Ranch(domAB1, 'GGGGGGGGGG', domAB2, 
-            chains = {domAB1:'A', domAB2:'B'}, symmetry=)
-        models2 = call_domAB1_linker_domAB2.run()
+        pass
 
     def test_input_files_created(self):
-
+        pass
 
 
     def test_pdbmodels_saved(self):
+
+        # Create PDBModels from pdb files
+        dom1 = B.PDBModel(
+            "/Users/guzmanfj/Documents/Stefan/multiprot/ranch_examples/1/2z6o_mod.pdb")
+        
+        dom2 = B.PDBModel(
+            "/Users/guzmanfj/Documents/Stefan/multiprot/ranch_examples/1/Histone_H3.pdb")
+        
+        domAB1 = B.PDBModel(
+            "/Users/guzmanfj/Documents/Stefan/multiprot/ranch_examples/4/dom1_AB.pdb")
+        
+        domAB2 = domAB1.clone()
+        
+        # Call ranch for exmaple 1 in ranch_examples
+        call_example1 = Ranch(dom1, 'GGGGGGGGGG', dom2)
+        models1 = call_example1.run()
+        
+        # Call ranch for example 4 in ranch_examples
+        call_example4 = Ranch(domAB1, 'GGGGGGGGGG', domAB2, 
+            chains = {domAB1:'A', domAB2:'B'})
+        models2 = call_example4.run()
+
+        # example 5 in ranch_examples
+        call_example5 = Ranch(domAB1, 'GGGGGGGGGG', domAB2, 
+            chains = {domAB1:'A', domAB2:'B'}, symmetry='p2', symtemplate=domAB1)
+        models3 = call_example5.run()
+
         # models lists contain 10 elements
         self.assertTrue(len(models1)==10, "models1 does not contain 10 elements")
-        self.assertTrue(len(models1)==10, "models2 does not contain 10 elements")
+        self.assertTrue(len(models2)==10, "models2 does not contain 10 elements")
+        self.assertTrue(len(models3)==10, "models3 does not contain 10 elements")
         
         # elements of models lists are PDBModels
         self.assertTrue(isinstance(models1[0], B.PDBModel), 
             "models1 contents are not PDBModels")
         self.assertTrue(isinstance(models2[0], B.PDBModel), 
             "models2 contents are not PDBModels")
+        self.assertTrue(isinstance(models3[0], B.PDBModel), 
+            "models1 contents are not PDBModels")
+
+
+
+class TestCleaning(BT.BiskitTest):
+    """ Test class for the cleaning methods post-run """
 
     def test_number_of_chains(self):
+        pass
 
 
     def test_order_amino_acids(self):
+        pass
 
 
 
---
+++ ranch_examples/trimer_multich/p3/fix_residues_v5.py
@@ -0,0 +1,176 @@
+import re
+
+filenames = []
+
+with open("filenames.txt") as f:
+	for line in f:
+		filenames.append(line.rstrip('\n'))
+
+# Count residues
+res_count = 0
+with open(filenames[0]) as f:
+	for line in f:
+		if re.search(r' CA ', line):
+			res_count += 1
+
+print 'Residue count:', res_count
+
+nchains = int(raw_input("Number of chains: "))
+chainsl = []	# list for chain lengths
+for i in range(nchains):
+	chainsl.append(int(raw_input('Length of chain ' + str(i+1) + ': ')))
+
+print ''
+
+for filename in filenames:
+
+	data = []
+
+	# Copy data from file
+	with open(filename) as f:
+		for line in f:
+			data.append(line.rstrip('\n'))
+
+	##print filename
+	##print 'ORIGINAL'
+	##for d in data[:20]:
+	##	print d
+	##print ''
+	
+	chains = []		# list for each chain atom entries
+	hetatm = []		# list for each chain hetatm entries
+	res_chain = []	# list for last residue in each chain
+	chain_identif = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O']	
+		# list for chain identifiers
+
+	index = 0		# index for the entry number
+
+	for i in range(nchains):
+		# chainsl.append(int(raw_input('Length of chain ' + str(i+1) + ': ')))
+		chains.append([])
+		hetatm.append([])
+		res_chain.append([0])
+
+		chainl = chainsl[i]		# length of the current chain
+		cacount = 0		# counts the residues
+
+		# Storing the chains in separate elements of 'chains' list,
+		# with their own chain identifier
+		for j in range(index,len(data)):
+			index += 1
+			
+			char = re.search(r'[A-Z0-9][A-Z0-9][A-Z0-9] [A-Z] ', data[j])
+			if char:
+				char = char.group()
+			else:
+				print data[j]
+				raise AttributeError("IDIOT")
+			# if NAP, RET, 22B, L2P, GLC add to hetatm list with the appropriate chain 
+			# identifier
+			if re.search(r'NAP|RET|22B|L2P|GLC', data[j]):
+				hetatm[i].append(data[j].replace(char, char[0:4]+chain_identif[i]+' '))
+			# if OXT and at the end of the chain, add to the chain list
+			elif re.search(r' OXT ', data[j]) and (cacount == chainl):
+				chains[i].append(data[j].replace(char, char[0:4]+chain_identif[i]+' '))
+			# if not OXT, add to the chain list
+			elif not re.search(r' OXT ', data[j]):
+				chains[i].append(data[j].replace(char, char[0:4]+chain_identif[i]+' '))
+			## TEST FOR OTHER CASES
+			
+			residue = data[j][17:20]
+			if re.search(r' CA ', data[j]):
+				cacount += 1
+			if cacount == chainl:
+				# if it is the last entry, break ... not sure about this one
+				if j == len(data)-1:
+					break
+				# if the next residue is different, and it is not a NAP atom
+				elif residue != data[j+1][17:20] and data[j+1][17:20] != 'NAP':
+					break 
+
+	'''
+	for i in range(nchains):
+		for d in chains[i][:20]:
+			print d
+	'''
+
+	n = 1		# initialising entry number
+
+	for i in range(nchains):		
+		# Replace entry number and residue sequence number
+		res_old = chains[i][0][23:26]	# initialize to the first residue number 
+										# (should be 1 in most cases)
+		res_new = 1
+		for j in range(len(chains[i])):
+
+			entry_num = re.search(r'ATOM\s+\d+', chains[i][j])
+			entry_res = re.search(r'[A-Z][A-Z][A-Z]\s[A-Z]\s+' + res_old, chains[i][j])
+			
+			if entry_res:
+				# replace residue number
+				chains[i][j] = chains[i][j].replace(entry_res.group(), 
+					entry_res.group()[0:5] + str(res_new).rjust(4))
+				# replace entry number
+				chains[i][j] = chains[i][j].replace(entry_num.group(), 
+					entry_num.group()[0:5] + str(n).rjust(6))
+				n += 1
+				continue
+			
+			# If the residue number changed (new residue)
+			res_old = chains[i][j][23:26]
+			res_new += 1
+			entry_res = re.search(r'[A-Z][A-Z][A-Z]\s[A-Z]\s+' + res_old, chains[i][j])
+			if entry_res:
+				# replace residue number
+				chains[i][j] = chains[i][j].replace(entry_res.group(), 
+					entry_res.group()[0:5] + str(res_new).rjust(4))
+				# replace entry number
+				chains[i][j] = chains[i][j].replace(entry_num.group(), 
+					entry_num.group()[0:5] + str(n).rjust(6))
+				n += 1
+			else:
+				print chains[i][j]
+				raise AttributeError(str(n) + " : chain " + str(i) + 
+					", entry " + str(j) + ", res_old: "+res_old)
+
+		# Add TER entry at the end of the chain
+		# FIX FOR CHAINS WITH MORE THAN 1000 RESIDUES
+		aa = re.search(r'[A-Z][A-Z][A-Z]\s[A-Z]\s+', chains[i][-1])
+		chains[i].append('TER' + str(n).rjust(8) + aa.group()[0:3].rjust(9) + 
+			aa.group()[4].center(3) + str(res_new))
+		n += 1
+
+		# last residue number in the current chain, plus 2 to add the hetatm entries
+		res_chain[i] = res_new+2
+	
+	# Join the chains in a single list
+	complete = []
+	for i in range(nchains):
+		complete += chains[i]
+
+	# Add HETATOM entries
+	for i in range(nchains):
+		res_new = res_chain[i]
+		for j in range(len(hetatm[i])):
+			entry_num = re.search(r'ATOM\s+\d+', hetatm[i][j])
+			entry_res = re.search(r'(NAP|RET|22B|L2P|GLC) [A-Z]\s+\d+', hetatm[i][j])
+
+			# replace residue number and entry number
+			complete.append(hetatm[i][j].replace(entry_res.group(), 
+				entry_res.group()[0:5] + str(res_new).rjust(4)).replace(entry_num.group(), 
+				'HETATM' + str(n).rjust(5)))
+			n += 1
+
+	##print ''
+	##print 'FIXED'
+	##for d in complete[:20]:
+	##	print d
+	##print "..."
+	##for d in complete[-20:]:
+	##	print d
+	##print ''
+
+	write_name = filename[:-4]+"_fixed.pdb"
+	with open(write_name,"w") as f:
+		for d in complete:
+			f.write(d + '\n')
