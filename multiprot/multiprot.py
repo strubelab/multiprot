9d563093f3dfcf961724c3c5b128f4ec87421a7c
Francisco Javier Guzman <36437485+javierguzm@users.noreply.github.com>
2018-08-15 16:49:23 +0300

    Created new file for multiprot tests, to be able to run multiprot.py as a script without the tests running every time.

--- multiprot/multiprot.py
+++ multiprot/multiprot.py
@@ -145,31 +145,3 @@ chains = create_args(args)
 call = builder.Builder(chains, args.destination, args.debug)
 models = call.build()
 
-
-#############
-##  TESTING        
-#############
-import testing
-
-class TestMultiprot(testing.AutoTest):
-    """
-    Test class
-
-    Test for the same examples as in ranch.py (1, 4, 5, 7, 10)
-    ONLY SINGLE CHAINS FOR NOW
-    """
-
-    def setUp(self):
-        linker = linker or 'G'*15
-        self.argstring1 = '--chain dom1.pdb '+linker+' dom2.pdb'
-        self.argstring4 = '--chain domAB1.pdb:A '+linker+' domAB1.pdb:B'
-        self.argstring5 = '--chain domAB1.pdb '+linker+' domAB2:A \
-        --symmetry p2 --symtemplate domAB1.pdb --poolsym s'
-        self.argstring7 = '--chain histone.pdb '+linker+' domAB1.pdb '+linker+
-            ' histone.pdb --symmetry p2 --symtemplate domAB1.pdb --poolsym mix'
-        self.argstring10 = '--chain domAB1.pdb '+linker+' domAB1.pdb:B '\
-            +linker+' domAB1.pdb:B'
-
-    def test_parsing(self):
-
-
